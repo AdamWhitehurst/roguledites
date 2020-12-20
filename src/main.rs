53d64316eb349ab5b4694913fcaf164efa9d3d87
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-12-20 14:56:54 -0800

    CHORE: init commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+
+fn main() {
+    let mut app = App::build();
+    app.add_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins);
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+    app.add_system(animate_sprite_system.system());
+    app.add_startup_system(setup.system()).run();
+}
+
+fn animate_sprite_system(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta_seconds());
+        if timer.finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+        }
+    }
+}
+
+fn setup(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("textures/roguledite.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 6, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands
+        .spawn(Camera2dBundle::default())
+        .spawn(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(0.1, true));
+}
